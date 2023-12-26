20c111172304279c48b36692f627d0e3ba8a2aa6
Agon Cecelia <agonn.c@gmail.com>
2023-12-26 14:41:18 +0100

    after

---
+++ after/plugin/colors.lua
@@ -0,0 +1,14 @@
+function SetMocha(color)
+	color = color or "catppuccin-mocha"
+	vim.cmd.colorscheme(color)
+end
+function SetFrappe(color)
+	color = color or "catppuccin-frappe"
+	vim.cmd.colorscheme(color)
+end
+function SetLatte(color)
+	color = color or "catppuccin-latte"
+	vim.cmd.colorscheme(color)
+end
+
+SetMocha()
