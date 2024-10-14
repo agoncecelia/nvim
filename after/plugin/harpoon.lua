a449cb4cf34b62db7239c3a34c53b292e93fb45b
Agon Cecelia <agonn.c@gmail.com>
2024-10-14 21:39:47 +0200

    add harpoon

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,5 +1,20 @@
+local harpoon = require("harpoon")
 
+-- REQUIRED
+harpoon:setup()
+-- REQUIRED
 
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+vim.keymap.set("n", "<leader>t", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+vim.keymap.set("n", "<leader>q", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<leader>w", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<leader>e", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<leader>r", function() harpoon:list():select(4) end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<leader>p", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<leader>n", function() harpoon:list():next() end)
 
 
 
--- lua/agonc/packer.lua
+++ lua/agonc/packer.lua
@@ -11,12 +11,17 @@ return require('packer').startup(function(use)
     'nvim-telescope/telescope.nvim',
     requires = { { 'nvim-lua/plenary.nvim' } }
   }
+  use "nvim-lua/plenary.nvim"
+  use {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    requires = { {"nvim-lua/plenary.nvim"} }
+}
 
   use { "catppuccin/nvim", as = "catppuccin" }
 
   use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
   use 'nvim-treesitter/playground'
-  use 'theprimeagen/harpoon'
   use 'mbbill/undotree'
   use 'tpope/vim-fugitive'
 
