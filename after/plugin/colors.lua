1d6e48ece2c697c7575d190cfee6eb7f4a324e25
Agon Cecelia <agonn.c@gmail.com>
2025-08-07 20:25:58 +0200

    change color theme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -2,17 +2,17 @@ Latte = "catppuccin-latte"
 Mocha = "catppuccin-mocha"
 Frappe = "catppuccin-frappe"
 function SetColor(color)
-    vim.cmd.colorscheme(color)
+	vim.cmd.colorscheme(color)
 end
 
 function ToggleColor()
-    if vim.g.colors_name == Latte then
-        SetColor(Mocha)
-    elseif vim.g.colors_name == Mocha then
-        SetColor(Frappe)
-    else
-        SetColor(Latte)
-    end
+	if vim.g.colors_name == Latte then
+		SetColor(Mocha)
+	elseif vim.g.colors_name == Mocha then
+		SetColor(Frappe)
+	else
+		SetColor(Latte)
+	end
 end
 
-SetColor(Latte)
+SetColor(Mocha)
